76f32e3eb0e4ab9a12313fe50d65a8b1a5956252
mbk7368 <mbk7368@gmail.com>
2024-10-04 16:13:06 +0300

    d

--- Departments.java
+++ Departments.java
@@ -1,32 +1,36 @@
-import java.util.Arraylist;
+import java.util.ArrayList;
 import java.util.List;
-
-
 
 class Departments{
 
+//////////Vars
     int ID;
     String Name;
     List<String> members = new ArrayList<>();
 
-
-
-    public Departments(int V1, String V2, String V3) {
+///////Constructor
+    public Departments(int V1, String V2) {
         this.ID = V1;
         this.Name = V2;
-        this.members = Stringlist.add(V3);
     }
 
+//////////Methods
+    public void addEmployee( String member) {
 
-    void addEmployee() {
-
+        this.members.add(member);
+    
     }
 
-    void removeEmployee() {
+    public void removeEmployee(String member) {
 
+        this.members.remove(member);
+    
     }
 
-    void getDepartmentDetails() {
+    public void getDepartmentDetails() {
+        System.out.println("Department ID:" + ID);
+        System.out.println("Department Name:" + Name);
+        System.out.println("Department Memebers:" + members);
 
     }
     
--- main.java
+++ main.java
@@ -2,9 +2,22 @@
 
 
 
-class HRM {
+class main {
     
     public static void main(String[] args) {
+
+        Departments itDept = new Departments(101, "IT");
+        
+        itDept.addEmployee("noor");
+        itDept.addEmployee("Daniel");
+        itDept.addEmployee("john");
+        itDept.addEmployee("tony");
+
+        itDept.removeEmployee("noor");
+        itDept.removeEmployee("tony");
+
+        itDept.getDepartmentDetails();
+
         
     }
 }
