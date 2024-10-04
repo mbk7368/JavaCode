96a916fb3819b30f44a220a9605b1c1f17a9f3bb
mbk7368 <mbk7368@gmail.com>
2024-10-04 16:26:23 +0300

    d

--- Employees.java
+++ Employees.java
@@ -1,60 +1,32 @@
+//Methods: promote(), demote(), increaseSalary(), displayDetails().
 
-import java.util.ArrayList;
-
-
-
-
-
-<<<<<<< HEAD:HRMgmtSystem.java
-class HRMgmtSystem {
-    public static void main(String[] args) {
-        
-    }
-
-}
-
-
-class Departments{
-    String Name;
-    String size;
-    List<string> accessibility = new ArrayList<>();
-
-
-}
 
 class Employees{
 
+    int id;
+    String name;
+    String age;
+    String department;
+    String role;
+    int salary;
 
 }
 
 
 //manager class
-=======
-class Employees{
-    
-    
-    
-    
-    //manager class
->>>>>>> c9eee09380f8927dd7219e16e5e47539046ec61d:Employees.java
 class Manager {
     
-
-
 
     public void addEmployee () {
         System.out.println("adding an employee by the manager");
     }
 }
 
-// regular class
 
+
+
+// regular class
 class Regular {
 
 }
 
-<<<<<<< HEAD:HRMgmtSystem.java
-
-=======
-}
->>>>>>> c9eee09380f8927dd7219e16e5e47539046ec61d:Employees.java
