1f56850928c8ad71a811b8662916479d95ba38b8
mbk7368 <mbk7368@gmail.com>
2024-10-08 13:25:16 +0300

    d

--- Departments.java
+++ Departments.java
@@ -6,15 +6,16 @@ class Departments{
 //////////Vars
     int ID;
     String Name;
+    int counter;
     List<String> members = new ArrayList<>();
-    List<Departments> DepartmentList = new ArrayList<>();
+    List<String> DepartmentList = new ArrayList<>();
 
 ///////Constructor
     public Departments(String V1) {
-        int counter = (1 + (int)DepartmentList.size());
+        counter = (1 + (int)DepartmentList.size());
         this.ID = this.counter;
         this.Name = V1;
-        DepartmentList.add(V1)
+        DepartmentList.add(V1);
         this.counter += 1;
     }
 
