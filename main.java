fd6bc1c1cda2f368ff5c17d3a7929f78031bb360
mbk7368 <mbk7368@gmail.com>
2024-10-18 22:32:52 +0300

    first commit

--- main.java
+++ main.java
@@ -5,8 +5,42 @@ class Main {
     
     public static void main(String[] args) {
 
+        //Creating Departments
         Departments itDept = new Departments("IT");
         itDept.mappingDepartments();
+        Departments HR = new Departments("Human Resources");
+        HR.mappingDepartments();
+        Departments Accounting = new Departments("Accounting");
+        Accounting.mappingDepartments();
+        Departments Marketing = new Departments("Marketing");
+        Marketing.mappingDepartments();
+
+        //Creating Roles
+        Roles CEO = new Roles("CEO");
+        CEO.mappingRoles();
+        Roles COO = new Roles("COO");
+        COO.mappingRoles();
+        Roles DepartmentManager = new Roles("Department Manager");
+        DepartmentManager.mappingRoles();
+        Roles ManagerAssisstant = new Roles("ManagerAssisstant");
+        ManagerAssisstant.mappingRoles();
+        Roles Supervisor = new Roles("Supervisor");
+        Supervisor.mappingRoles();
+        Roles Specialist = new Roles("Specialist");
+        Specialist.mappingRoles();
+        Roles Worker = new Roles("Worker");
+        Worker.mappingRoles();
+
+        //Creating HRManager
+
+        HRManager SarahWalker = new HRManager ( "Sarah Walker", 5000.0);
+        SarahWalker.MappingHRManager();
+        HRManager RobertSapolsky = new HRManager ( "Robert Sapolsky", 5000.0);
+        RobertSapolsky.MappingHRManager();
+
+        //Creating Employees
+
+
 
         Employee JohnMane = new Employee("John Mane", 28, "IT", "Specialist", 4000, 2);
         JohnMane.mappingEmployees();
@@ -17,11 +51,7 @@ class Main {
         RegularEmployee MattDaemon = new RegularEmployee ("Matt Daemon", 38, "IT", "Worker", 3000, 1);
         MattDaemon.mappingEmployees();
 
-        HRManager SarahWalker = new HRManager ( "Sarah Walker", 5000.0);
-        SarahWalker.MappingHRManager();
 
-        Roles Specialist = new Roles("Specialist");
-        Specialist.mappingRoles();
 
 
 
